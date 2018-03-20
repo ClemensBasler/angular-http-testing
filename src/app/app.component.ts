61ca72459d3e364447ac70389353e0d92df2cf05
Clemens Basler <clemens-basler@t-online.de>
2018-03-20 16:26:56 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  results = '';
+  dataset = this.getJson();
+  constructor(private http: HttpClient){
+  }
+  ngOnInit(): void {
+    this.http.get('http://localhost:8000/json').subscribe(data => {
+      console.log(data);
+
+    });
+  }
+
+  getJson(): string{
+    this.http.get('http://localhost:8000/time').subscribe(data => {
+      console.log(data.time);
+      this.dataset = data.time;
+  }
 }
